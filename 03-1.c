cc7b8db65f9b30641184d073565303b2a86aa942
sewadzhan <skhan_1@edu.hse.ru>
2021-02-01 15:00:53 +0300

    Lab 3 done.

--- 03-1.c
+++ 03-1.c
@@ -4,23 +4,19 @@
 
 int main()
 {
+  //This program creates 3 children from one parent process
   pid_t pid, ppid;
   int a = 0;
-
-  fork();
-  //
-  // If the new process is created successfully,
-  // two processes start working in pseudo-parallel from this point: the old and the new one.
-  //
-  // Before the following expression is executed,
-  // the value of the variable a in both processes is 0.
-  //
-  a = a+1;
-
+  
+  while	(a < 3) {
+	fork();
+  	a = a+1;
+  }	
+  
   pid  = getpid();
   ppid = getppid();
 
-  printf("My pid = %d, my ppid = %d, result = %d\n", (int)pid, (int)ppid, a);
+  printf("This pid = %d, this ppid = %d\n", (int)pid, (int)ppid);
 
   return 0;
 }
