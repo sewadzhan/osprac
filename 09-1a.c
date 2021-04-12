dee0703870eee3931f9ea4a9ece341325de1f49e
sewadzhan <skhan_1@edu.hse.ru>
2021-04-12 16:49:55 +0300

    Lab 9 done

--- 09-1a.c
+++ 09-1a.c
@@ -1,45 +1,104 @@
 #include <sys/types.h>
 #include <sys/ipc.h>
+#include <sys/shm.h>
 #include <sys/sem.h>
 #include <stdio.h>
+#include <errno.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[], char *envp[])
+int main()
 {
-  int    semid;        // IPC descriptor for an array of IPC semaphores
-  char   pathname[]="09-1a.c"; // The file name used to generate the key.
-                               // A file with this name must exist in the current directory
-  key_t  key;          // IPC key
-  struct sembuf mybuf; // Structure for specifying operations on a semaphore.
+  int     *array;
+  int     semid;        
+  struct  sembuf mybuf; 
+  int     shmid;
+  int     new = 1;
+  char    pathname[] = "09-1a.c";
+  key_t   key;
+  long    i;
 
   if ((key = ftok(pathname,0)) < 0) {
     printf("Can\'t generate key\n");
     exit(-1);
   }
-  //
-  // Try to access by key the array of semaphores, if it exists,
-  // or create it from a single semaphore if it does not exist yet,
-  // with read & write access for all users.
-  //
-  if ((semid = semget(key, 1, 0666 | IPC_CREAT)) < 0) {
-    printf("Can\'t create semaphore set\n");
-    exit(-1);
+
+  if ((shmid = shmget(key, 3*sizeof(int), 0666|IPC_CREAT|IPC_EXCL)) < 0) {
+    if (errno != EEXIST) {
+      printf("Can\'t create shared memory\n");
+      exit(-1);
+    } else {
+      if ((shmid = shmget(key, 3*sizeof(int), 0)) < 0) {
+        printf("Can\'t find shared memory\n");
+        exit(-2);
+      }
+      new = 0;
+    }
   }
-  //
-  // Perform operation D(semid1,1) on the semaphore array.
-  // But first fill in the structure. The flag, as usual, is set to 0.
-  // The semaphore array consists of one semaphore with number 0.
-  // The opcode is -1.
-  //
+
+  if ((semid = semget(key, 1, 0666 | IPC_CREAT | IPC_EXCL)) < 0) {
+      if (errno != EEXIST) {
+          printf("An unexpected error occured. The semaphore does not exist.\n");
+          exit(-3);
+      }
+      else {
+          if ((semid = semget(key, 1, 0)) < 0) {
+              printf("Unable to get semaphore by key.\n");
+              exit(-4);
+          }
+      }
+  }
+  else {
+      mybuf.sem_num = 0;
+      mybuf.sem_op = 1;
+      mybuf.sem_flg = 0;
+
+      if (semop(semid, &mybuf, 1) < 0) {
+          printf("Can\'t set original value of this semaphore to 1.\n");
+          exit(-5);
+      }
+  }
+
+  if ((array = (int *)shmat(shmid, NULL, 0)) == (int *)(-1)) {
+    printf("Can't attach shared memory in program A.\n");
+    exit(-6);
+  }
+
   mybuf.sem_num = 0;
-  mybuf.sem_op  = -1;
+  mybuf.sem_op = -1;
   mybuf.sem_flg = 0;
 
   if (semop(semid, &mybuf, 1) < 0) {
-    printf("Can\'t wait for condition\n");
-    exit(-1);
+      printf("Can\'t enter the critical section properly in program A.\n");
+      exit(-7);
   }
 
-  printf("The condition is present\n");
+  if (new) {
+    array[0] =  1;
+    array[1] =  0;
+    array[2] =  1;
+  } else {
+    array[0] += 1;
+    for(i=0; i<2000000000L; i++);
+    array[2] += 1;
+  }
+
+  printf
+    ("Program A was spawned %d times, program B - %d times, total - %d times\n",
+    array[0], array[1], array[2]);
+
+  mybuf.sem_num = 0;
+  mybuf.sem_op = 1;
+  mybuf.sem_flg = 0;
+
+  if (semop(semid, &mybuf, 1) < 0) {
+      printf("Can\'t exit the critical section properly in program A.\n");
+      exit(-8);
+  }
+
+  if (shmdt(array) < 0) {
+    printf("Can't detach shared memory in program A.\n");
+    exit(-9);
+  }
+
   return 0;
 }
--- 09-1b.c
+++ 09-1b.c
@@ -1,40 +1,104 @@
 #include <sys/types.h>
 #include <sys/ipc.h>
+#include <sys/shm.h>
 #include <sys/sem.h>
 #include <stdio.h>
+#include <errno.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[], char *envp[])
+int main()
 {
-  int   semid;
-  char pathname[]="09-1a.c";
-  key_t key;
-  struct sembuf mybuf;
+  int     *array;
+  int     shmid;
+  int     new = 1;
+  int     semid;        
+  struct  sembuf mybuf; 
+  char    pathname[] = "09-1a.c";
+  key_t   key;
+  long    i;
 
   if ((key = ftok(pathname,0)) < 0) {
     printf("Can\'t generate key\n");
     exit(-1);
   }
 
-  if ((semid = semget(key, 1, 0666 | IPC_CREAT)) < 0) {
-    printf("Can\'t create semaphore set\n");
-    exit(-1);
+  if ((shmid = shmget(key, 3*sizeof(int), 0666|IPC_CREAT|IPC_EXCL)) < 0) {
+    if (errno != EEXIST) {
+      printf("Can\'t create shared memory\n");
+      exit(-2);
+    } else {
+      if ((shmid = shmget(key, 3*sizeof(int), 0)) < 0) {
+        printf("Can\'t find shared memory\n");
+        exit(-3);
+      }
+      new = 0;
+    }
   }
-  //
-  // Perform operation A(semid1,1) on the semaphore array.
-  // But first fill in the structure. The flag, as usual, is set to 0.
-  // The semaphore array consists of one semaphore with number 0.
-  // The opcode is 1.
-  //
+
+  if ((semid = semget(key, 1, 0666 | IPC_CREAT | IPC_EXCL)) < 0) {
+      if (errno != EEXIST) {
+          printf("An unexpected error occured. The semaphore does not exist.\n");
+          exit(-4);
+      }
+      else {
+          if ((semid = semget(key, 1, 0)) < 0) {
+              printf("Unable to get semaphore by key.\n");
+              exit(-5);
+          }
+      }
+  }
+  else {
+      mybuf.sem_num = 0;
+      mybuf.sem_op = 1;
+      mybuf.sem_flg = 0;
+
+      if (semop(semid, &mybuf, 1) < 0) {
+          printf("Can\'t set original value of this semaphore to 1.\n");
+          exit(-6);
+      }
+  }
+
+  if ((array = (int *)shmat(shmid, NULL, 0)) == (int *)(-1)) {
+    printf("Can't attach shared memory in program B.\n");
+    exit(-7);
+  }
+
   mybuf.sem_num = 0;
-  mybuf.sem_op  = 1;
+  mybuf.sem_op = -1;
   mybuf.sem_flg = 0;
 
   if (semop(semid, &mybuf, 1) < 0) {
-    printf("Can\'t add 1 to semaphore\n");
-    exit(-1);
+      printf("Can\'t enter the critical section properly in program B.\n");
+      exit(-8);
   }
 
-  printf("The condition is present\n");
+  if (new) {
+    array[0] =  0;
+    array[1] =  1;
+    array[2] =  1;
+  } else {
+    array[1] += 1;
+    for(i=0; i<2000000000L; i++);
+    array[2] += 1;
+  }
+
+  printf
+    ("Program A was spawned %d times, program B - %d times, total - %d times\n",
+    array[0], array[1], array[2]);
+
+  mybuf.sem_num = 0;
+  mybuf.sem_op = 1;
+  mybuf.sem_flg = 0;
+
+  if (semop(semid, &mybuf, 1) < 0) {
+      printf("Can\'t exit the critical section properly in program B.\n");
+      exit(-8);
+  }
+
+  if (shmdt(array) < 0) {
+    printf("Can't detach shared memory in program B.\n");
+    exit(-9);
+  }
+
   return 0;
 }
